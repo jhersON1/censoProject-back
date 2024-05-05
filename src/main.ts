43a1e467b6ce8682aaf094c71de4c311c0f58655
jherson <rodriguezpaul98@gmail.com>
2024-05-04 22:52:12 -0400

    auth, dokerize app and data base configuration finished

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,24 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { Logger, ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  app.enableCors();
+
+  const logger = new Logger('Bootstrap');
+
+  app.setGlobalPrefix('api');
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+    }),
+  );
+
+  await app.listen(process.env.PORT ?? 3000);
+  logger.log(`App running on port ${process.env.PORT}`);
 }
 bootstrap();
